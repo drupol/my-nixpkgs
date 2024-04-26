1ab8ac2358039bacc0de0864fedccb19ae4d0194
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-04-26 21:14:14 +0200

    fix: remove leftovers

--- imports/pkgs-all.nix
+++ imports/pkgs-all.nix
@@ -1,18 +1,9 @@
-{ inputs, lib, ... }:
+{ ... }:
 {
 
   perSystem =
+    { ... }:
     {
-      config,
-      self',
-      inputs',
-      pkgs,
-      system,
-      ...
-    }:
-    {
-      packages = {
-        scikit-learn-1-3-2 = pkgs.callPackage ../pkgs/scikit-learn-1-3-2 { };
-      };
+      packages = { };
     };
 }
