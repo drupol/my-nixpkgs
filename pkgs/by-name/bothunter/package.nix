a24a2bdab48d9035a75f571ea00738b08d119cee
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-01 12:24:54 +0200

    bothunter: use original repo and remote patches

--- pkgs/by-name/bothunter/package.nix
+++ pkgs/by-name/bothunter/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchpatch,
   scikit-learn-1-2,
 }:
 
@@ -9,15 +10,30 @@ let
 in
 scikit-learn-1-2.python3.pkgs.buildPythonApplication rec {
   pname = "bothunter";
-  version = "e2f95516e675b6a9a37423f7517ecbd06d21ad86";
+  version = "fc42285d3e329bcb7f3122b58eb6a3855e5d7124";
   format = "pyproject";
 
   src = fetchFromGitHub {
-    owner = "drupol";
+    owner = "ahmad-abdellatif";
     repo = "bothunter";
     rev = version;
-    hash = "sha256-rH3juXroZM8XQhRlnFms3oM0xbRKPKHNlSx7IjuJ/9U=";
+    hash = "sha256-fHi6GpIZgUJIkfnn70XVDApvD4sMtiyugNxpVunM3xA=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/ahmad-abdellatif/BotHunter/pull/3/commits/bff867a4ab52b54ca56138fc218662bb221972f0.patch";
+      hash = "sha256-sP0owZMS9Vfl72iVexmVrr+Tip70+fvPI0TUW/AKD/g=";
+    })
+    (fetchpatch {
+      url = "https://github.com/ahmad-abdellatif/BotHunter/pull/3/commits/096e23f2a62ef72da651493c79b5cabbd3039c09.patch";
+      hash = "sha256-hZquK2qnyJsJyyXLFlvMfDFmQyg56NUGqdyPr1rh+Ok=";
+    })
+    (fetchpatch {
+      url = "https://github.com/ahmad-abdellatif/BotHunter/pull/3/commits/e2f95516e675b6a9a37423f7517ecbd06d21ad86.patch";
+      hash = "sha256-Yz06PH0OhLnRuHtcDdtSTDOUvPxF779Qk1PvVXqAkZ0=";
+    })
+  ];
 
   nativeBuildInputs = [
     python3.pkgs.setuptools
